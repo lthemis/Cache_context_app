6b13aa8e861a6589d35bc45c43027fc88a36de07
lthemis <lukasz.tylke@gmail.com>
2022-05-18 15:17:14 +0200

    feat: basic server

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use('*', (req, res) => res.sendStatus(404));
+
+async function start() {
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+}
